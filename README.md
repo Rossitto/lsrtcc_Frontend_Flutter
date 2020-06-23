33227ffd3804308fc0e29bc792bbe763c10575f9
Rossitto <lucas74rossi@gmail.com>
2020-06-22 21:35:10 -0300

    added logo.png and refactored package imports

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
 # lsrtcc_flutter
 
-A new Flutter project.
+Frontend of the application built for my final paper on Specialization in Software Engineering at UFPR.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,63 @@
+import 'package:lsrtcc_flutter/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:lsrtcc_flutter/components/rounded_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration,
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintText: 'Enter your password',
+              ),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            RoundedButton(
+              color: Colors.lightBlueAccent,
+              onPressed: null,
+              text: 'Log In',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
