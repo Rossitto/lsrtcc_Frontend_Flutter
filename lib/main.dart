42e8aa91fdfe5b292dab664ee30b24312613c98c
Lucas Rossi <lucas74rossi@gmail.com>
2021-04-21 18:50:58 -0300

    welcome screen depending on user logged in or not

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,23 @@ import 'package:lsrtcc_flutter/screens/registerPub_screen.dart';
 import 'package:lsrtcc_flutter/screens/welcome_screen.dart';
 import 'package:lsrtcc_flutter/screens/login_screen.dart';
 import 'package:lsrtcc_flutter/screens/registration_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/welcome_screen_debug.dart';
 
-// void main() => runApp(LsrTcc());
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  String userInfo = prefs.getString('userLoggedInResponseBody');
+  var isLoggedIn = userInfo == null ? false : true;
   Intl.defaultLocale = "pt_BR";
-  initializeDateFormatting().then((_) => runApp(LsrTcc()));
-}
-
-class LsrTcc extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  initializeDateFormatting();
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: WelcomeScreen.id,
+      initialRoute: isLoggedIn ? ProfileScreen.id : WelcomeScreen.id,
       routes: {
         WelcomeScreenDebug.id: (context) => WelcomeScreenDebug(),
         WelcomeScreen.id: (context) => WelcomeScreen(),
@@ -36,6 +35,33 @@ class LsrTcc extends StatelessWidget {
         CalendarScreen.id: (context) => CalendarScreen(),
         ProfileScreen.id: (context) => ProfileScreen(),
       },
-    );
-  }
+    ),
+  );
 }
+
+// void main() => runApp(LsrTcc());
+
+// void main() {
+//   Intl.defaultLocale = "pt_BR";
+//   initializeDateFormatting().then((_) => runApp(LsrTcc()));
+// }
+
+// class LsrTcc extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       initialRoute: WelcomeScreen.id,
+//       routes: {
+//         WelcomeScreenDebug.id: (context) => WelcomeScreenDebug(),
+//         WelcomeScreen.id: (context) => WelcomeScreen(),
+//         LoginScreen.id: (context) => LoginScreen(),
+//         RegistrationScreen.id: (context) => RegistrationScreen(),
+//         RegisterBandScreen.id: (context) => RegisterBandScreen(),
+//         RegisterPubScreen.id: (context) => RegisterPubScreen(),
+//         CalendarScreen.id: (context) => CalendarScreen(),
+//         ProfileScreen.id: (context) => ProfileScreen(),
+//       },
+//     );
+//   }
+// }
